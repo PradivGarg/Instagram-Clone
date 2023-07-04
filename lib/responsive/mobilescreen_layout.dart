b220b129fa0711cc4141c03db032958b8287950e
Pradiv Garg <pradiv1994@rediffmail.com>
2023-07-04 16:51:09 +0530

    page controller and design controller

--- lib/responsive/mobilescreen_layout.dart
+++ lib/responsive/mobilescreen_layout.dart
@@ -1,5 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sociagram/utils/colors.dart';
@@ -27,35 +27,70 @@ class _MobileScreenLayoutState extends State<mobileScreenLayout> {
   //     username = (snap.data() as Map<String, dynamic>)['username'];
   //   });
   // }
+  int _page = 0;
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    pageController.dispose();
+  }
+
+  void navigationTapped(int page) {
+    pageController.jumpToPage(page);
+  }
+
+  void onPageChanged(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('This is mobile'),
+      body: PageView(
+        children: [],
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        onPageChanged: onPageChanged,
       ),
       bottomNavigationBar: CupertinoTabBar(
         backgroundColor: mobileBackgroundColor,
-        items: const [
+        items: [
           BottomNavigationBarItem(
-              icon: Icon(Icons.home), label: '', backgroundColor: primaryColor),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search),
+              icon: Icon(Icons.home,
+                  color: _page == 0 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(Icons.add_circle),
+              icon: Icon(Icons.search,
+                  color: _page == 1 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(Icons.favorite),
+              icon: Icon(Icons.add_circle,
+                  color: _page == 2 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor),
           BottomNavigationBarItem(
-              icon: Icon(Icons.person),
+              icon: Icon(Icons.favorite,
+                  color: _page == 3 ? primaryColor : secondaryColor),
+              label: '',
+              backgroundColor: primaryColor),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person,
+                  color: _page == 4 ? primaryColor : secondaryColor),
               label: '',
               backgroundColor: primaryColor)
         ],
+        onTap: navigationTapped,
       ),
     );
   }
